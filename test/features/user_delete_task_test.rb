ce48daf740d04c05594b575ca95365545d0d146b
Rick Bacci <rickbacci@gmail.com>
2015-05-24 00:55:13 -0600

    fix hash data

--- test/features/user_delete_task_test.rb
+++ test/features/user_delete_task_test.rb
@@ -4,8 +4,8 @@ class DeleteTaskTest < FeatureTest
 
   def test_delete_a_task
     TaskManager.destroy_database
-    TaskManager.create(title: "Practice Capybara",
-                       description: 'tonight')
+    TaskManager.create(title: "Practice Capybara", description: 'tonight')
+
     visit '/tasks'
     click_on "Delete"
 
