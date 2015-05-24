218f1822a9cdf2bade26352a2d4fba526ef775fc
Rick Bacci <rickbacci@gmail.com>
2015-05-23 23:03:39 -0600

    rename test file

---
+++ test/features/user_edit_task_test.rb
@@ -0,0 +1,23 @@
+require_relative '../test_helper.rb'
+
+class EditTaskTest < FeatureTest
+
+  def test_user_can_edit_task
+    TaskManager.destroy_database
+    TaskManager.create(title: "Practice Capybara",
+                       description: 'tonight')
+    visit '/tasks'
+    click_link('Edit')
+    
+    task = TaskManager.all.last
+    assert_equal "/tasks/#{task.id}/edit", current_path
+   
+    fill_in "task[title]", with: "tomorrow"
+    fill_in "task[description]", with: "sequel"
+    click_link_or_button "Update Task"
+    
+    assert page.has_content?("tomorrow")
+    assert page.has_content?("sequel")
+  end
+
+end
